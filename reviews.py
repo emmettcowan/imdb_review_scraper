a657f7e28af6b311a6056fb40b1922bfff316431
Emmett Cowan <knockanillaun@gmail.com>
2020-09-10 20:09:08 +0100

    Edited git ignore file

--- reviews.py
+++ reviews.py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 ratings = []
-seasons = 4
+seasons = 17
 max_episode_count = 0
 
 
 def gen_ratings_data():
     for x in range(1, seasons+1):
-        url = f'https://www.imdb.com/title/tt0096697/episodes?season={x}&ref_=tt_eps_sn_31'
+        url = f'https://www.imdb.com/title/tt1086761/episodes?season={x}&ref_=tt_eps_sn_31'
         response = get(url)
         html_soup = BeautifulSoup(response.text, 'html.parser')
         episode_containers = html_soup.find_all('div', {"class": ["list_item even", "list_item odd"]} )
@@ -38,6 +38,7 @@ def gen_csv():
 
 
 def gen_heatmap():
+    #function to crate graph unfinished
     x = []
     y = []
 
@@ -57,4 +58,3 @@ def gen_heatmap():
 
 gen_ratings_data()
 gen_csv()
-gen_heatmap()
